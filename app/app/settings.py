51c5fe363b48b4326e7bc52f2c712f10496d5ff4
Spiridoula <smenounou@gmail.com>
2022-04-20 19:20:46 +0300

    Added Key Security

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gwbi4wx6ql)y18itx@^6epc6t4o^p!$3$d2(^s1mzdwp^5!j2v'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
